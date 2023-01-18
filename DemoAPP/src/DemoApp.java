17aeb50b72d5b17460232e6a33810646a5c1ec4a
Muzammil <muzammilmm290@gmail.com>
2023-01-18 23:33:38 +0530

    `first commit

---
+++ DemoAPP/src/DemoApp.java
@@ -0,0 +1,18 @@
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [] a= {10,20,30,40};
+		for(int ele:a)
+		{
+			System.out.println(ele);
+		}
+		
+		String name ="sachin";
+		int count=name.toUpperCase().length();
+		System.out.println("the length of the string is:"+count);
+	}
+
+}
